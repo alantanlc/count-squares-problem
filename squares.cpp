ea24728de1c69d423b5a1220c0d241878b4a5214
Alan Tan <alanwuha91@gmail.com>
2019-03-16 20:59:11 +0800

    fixed error in isPerpendicular conditions and added missing abs() when comparing with epilson

--- squares.cpp
+++ squares.cpp
@@ -2,10 +2,11 @@
 #include <vector>
 #include <limits>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
-// A Point/Coordinate is defined as a pair of doubles (x, y)
+// A Point (aka coordinate) is defined as a pair of doubles (x, y)
 class Point {
 public:
 	double x, y;
@@ -32,26 +33,28 @@ public:
 
 	// Checks if this line and given line are parallel using gradient
 	bool isParallel(Line l) {
-		string isParallel = ((this->getGradient() - l.getGradient() < numeric_limits<double>::epsilon())) ? "true" : "false";
-		cout << "isParallel(): gradients = (" << this->getGradient() << ", " << l.getGradient() << "), parallel = " << isParallel << "\n";
+		string isParallel = 
+			(abs(this->getGradient() - l.getGradient()) < numeric_limits<double>::epsilon()) ? "true" : "false";
+		cout << "isParallel(): gradients = (" << this->getGradient() 
+			<< ", " << l.getGradient() << "), parallel = " << isParallel << "\n";
 
-		return (this->getGradient() - l.getGradient() < numeric_limits<double>::epsilon());
+		return (abs(this->getGradient() - l.getGradient()) < numeric_limits<double>::epsilon());
 	}
 
 	// Checks if this line is vertical
 	bool isVertical() {
-		return this->getGradient() - numeric_limits<double>::max() < numeric_limits<double>::epsilon();
+		return abs(this->getGradient() - numeric_limits<double>::max()) < numeric_limits<double>::epsilon();
 	}
 
 	// Checks if this line is horizontal
 	bool isHorizontal() {
-		return this->getGradient() < numeric_limits<double>::epsilon();
+		return abs(this->getGradient()) < numeric_limits<double>::epsilon();
 	}
 
 	// Computes gradient of this line
 	double getGradient() {
 		// gradient = (y2 - y1) / (x2 - x1);
-		if(this->p2.x - this->p1.x > numeric_limits<double>::epsilon()) {
+		if(abs(this->p2.x - this->p1.x) > numeric_limits<double>::epsilon()) {
 			return (double) (this->p2.y - this->p1.y) / (double) (this->p2.x - this->p1.x);
 		}
 
@@ -76,33 +79,29 @@ public:
 	// Checks if this pair of parallel lines and given pair of parallel lines
 	// forms a square between intersection points
 	bool isSquare(ParallelLines p) {
-		if(this->isPerpendicular(p) && this->hasSamePerpendicularDistanceBetweenLines(p)) {
-			return true;
-		}
-
-		return false;
+		return this->isPerpendicular(p) && this->hasSamePerpendicularDistanceBetweenLines(p);
 	}
 
 	// Checks if this pair of parallel lines and given pair of parallel lines
 	// are perpendicular
 	bool isPerpendicular(ParallelLines p) {
 		// If lines are perpendicular, then gradient g1 * g2 equals -1
-		// or both Line l1 are vertical
-		// or both Line l1 are horizontal
-		return (this->l1.getGradient() * p.l1.getGradient() + 1.0) < numeric_limits<double>::epsilon()
-			|| this->l1.isVertical() && p.l1.isVertical()
-			|| this->l1.isHorizontal() && p.l1.isHorizontal();
+		// or if one Line l1 is vertical while the other is horizontal, vice versa
+		return abs(this->l1.getGradient() * p.l1.getGradient() + 1.0) < numeric_limits<double>::epsilon()
+			|| this->l1.isVertical() && p.l1.isHorizontal()
+			|| this->l1.isHorizontal() && p.l1.isVertical();
 	}
 
 	// Checks if the 2 sets of parallel lines has same perpendicular distance between their lines l1 and l2.
 	bool hasSamePerpendicularDistanceBetweenLines(ParallelLines p) {
 		// For each pair of parallel lines, compute perpendicular distance between lines l1 and l2.
 		// Check if the perpendicular distances are the same for the two pairs of parallel lines.
-		return (this->getPerpendicularDistance() - p.getPerpendicularDistance()) < numeric_limits<double>::epsilon();
+		return abs(this->getPerpendicularDistance() - p.getPerpendicularDistance()) < numeric_limits<double>::epsilon();
 	}
 
 	// Compute perpendicular distance between lines l1 and l2
 	double getPerpendicularDistance() {
+		// HELP!
 		// Need to compute perpendicular distance between this->l1 and this->l2
 		// ...
 
@@ -152,17 +151,26 @@ int findSquares(vector<ParallelLines> parallelLines) {
 }
 
 int main(int argc, char** argv) {
+	// Initialize points
 	Point p1 { 0.0, 0.0 };
 	Point p2 { 0.0, 1.0 };
-
 	Point p3 { 1.0, 0.0 };
 	Point p4 { 1.0, 1.0 };
 
 	Point p5 { 0.0, 0.0 };
 	Point p6 { 1.0, 0.0 };
-
 	Point p7 { 0.0, 1.0 };
 	Point p8 { 1.0, 1.0 };
+
+	Point p9  { 0.0, 1.0 };
+	Point p10 { 1.0, 0.0 };
+	Point p11 { -1.0, 0.0 };
+	Point p12 { 0.0, -1.0 };
+
+	Point p13 { -1.0, 0.0 };
+	Point p14 { 0.0, 1.0 };
+	Point p15 { 0.0, -1.0 };
+	Point p16 { 1.0, 0.0 };
 
 	// Lines l1 and l2 are parallel lines p1
 	Line l1 { p1, p2 };
@@ -172,9 +180,18 @@ int main(int argc, char** argv) {
 	Line l3 { p5, p6 };
 	Line l4 { p7, p8 };
 
+	// Lines l1 and l2 are parallel lines p1
+	Line l5 { p9, p10 };
+	Line l6 { p11, p12 };
+
+	// Lines l3 and l4 are parallel lines p2
+	Line l7 { p13, p14 };
+	Line l8 { p15, p16 };
+
 	// Initialize lines
 	// Parallel lines p1 and p2 form a square
 	vector<Line> lines { l1, l2, l3, l4 };
+	// vector<Line> lines { l1, l2, l3, l4, l5, l6, l7, l8 };
 
 	// Find all pairs of parallel lines
 	cout << "Find parallel lines: \n";
