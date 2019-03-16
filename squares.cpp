427a0852e22ee7f034c0bb66cedccb695aa3a01a
Alan Tan <alanwuha91@gmail.com>
2019-03-17 00:55:33 +0800

    created countSquares() function

--- squares.cpp
+++ squares.cpp
@@ -6,6 +6,10 @@
 
 using namespace std;
 
+const double MAX = numeric_limits<double>::max();
+const double EPSILON = numeric_limits<double>::epsilon();
+const bool IS_PRINT_LOGS = false;
+
 // A Point (aka coordinate) is defined as a pair of doubles (x, y)
 class Point {
 public:
@@ -23,12 +27,12 @@ public:
 	}
 
 	bool isValid() {
-		return !abs(this->x - numeric_limits<double>::max()) < numeric_limits<double>::epsilon()
-			&& !abs(this->y - numeric_limits<double>::max()) < numeric_limits<double>::epsilon();
+		return !abs(this->x - MAX) < EPSILON
+			&& !abs(this->y - MAX) < EPSILON;
 	}
 
 	toString() {
-		cout << "(" << x << ", " << y << ")\n";
+		cout << "(" << x << ", " << y << ") ";
 	}
 };
 
@@ -46,49 +50,43 @@ public:
 
 	// Checks if this line and given line are parallel using gradient
 	bool isParallel(Line l) {
-		string isParallel = 
-			(abs(this->getGradient() - l.getGradient()) < numeric_limits<double>::epsilon()) ? "true" : "false";
-		cout << "isParallel(): gradients = (" << this->getGradient() 
-			<< ", " << l.getGradient() << "), parallel = " << isParallel << "\n";
-
-		return (abs(this->getGradient() - l.getGradient()) < numeric_limits<double>::epsilon());
+		return (abs(this->getGradient() - l.getGradient()) < EPSILON);
 	}
 
 	// Checks if this line and given line are perpendicular using gradient
 	// If lines are perpendicular, then gradient g1 * g2 equals -1
 	// or if one Line l1 is vertical while the other is horizontal, vice versa
 	bool isPerpendicular(Line l) {
-		return abs(this->getGradient() * l.getGradient() + 1.0) < numeric_limits<double>::epsilon()
+		return abs(this->getGradient() * l.getGradient() + 1.0) < EPSILON
 			|| this->isVertical() && l.isHorizontal()
 			|| this->isHorizontal() && l.isVertical();
 	}
 
 	// Checks if this line is vertical
 	bool isVertical() {
-		return abs(this->getGradient() - numeric_limits<double>::max()) < numeric_limits<double>::epsilon();
+		return abs(this->getGradient() - MAX) < EPSILON;
 	}
 
 	// Checks if this line is horizontal
 	bool isHorizontal() {
-		return abs(this->getGradient()) < numeric_limits<double>::epsilon();
+		return abs(this->getGradient()) < EPSILON;
 	}
 
 	// Computes gradient of this line
-	// gradient = (y2 - y1) / (x2 - x1);
-	// if x2 - x1 equals zero, return "infinity"
+	// gradient = (y2 - y1) / (x2 - x1). if x2 - x1 equals zero, return MAX.
 	double getGradient() {
-		if(abs(this->p2.x - this->p1.x) > numeric_limits<double>::epsilon()) {
+		if(abs(this->p2.x - this->p1.x) > EPSILON) {
 			return (double) (this->p2.y - this->p1.y) / (double) (this->p2.x - this->p1.x);
 		}
 
-		return numeric_limits<double>::max();
+		return MAX;
 	}
 
 	// Computes y-axis intersection of this line
 	// c = y - mx
 	double getC() {
 		if(this->isVertical()) {
-			return numeric_limits<double>::max();
+			return MAX;
 		} else if(this->isHorizontal()) {
 			return this->p1.y;
 		}
@@ -100,8 +98,8 @@ public:
 	// x = (c2 - c1) / (m1 - m2)
 	// y = m1 * x + c1
 	Point getIntersectionPoint(Line l) {
-		double x = numeric_limits<double>::max();
-		double y = numeric_limits<double>::max();
+		double x = MAX;
+		double y = MAX;
 
 		if(this->isVertical() && l.isHorizontal()) {
 			x = this->p1.x;
@@ -140,38 +138,38 @@ public:
 		Point p3 = this->l2.getIntersectionPoint(p.l1);
 		Point p4 = this->l2.getIntersectionPoint(p.l2);
 
-		cout << "Intersection points: " << endl;
-		p1.toString();
-		p2.toString();
-		p3.toString();
-		p4.toString();
-		cout << endl;
+		if(IS_PRINT_LOGS) {
+			cout << "Intersection points: " << endl;
+			p1.toString();
+			p2.toString();
+			p3.toString();
+			p4.toString();
+			cout << "\n";
 
-		cout << "Euclidean distances: " << endl;
-		cout << p1.getEuclideanDistance(p2) << endl;
-		cout << p1.getEuclideanDistance(p3) << endl;
-		cout << p1.getEuclideanDistance(p4) << endl;
-		cout << endl;
+			cout << "Euclidean distances: [";
+			cout << p1.getEuclideanDistance(p2) << ", ";
+			cout << p1.getEuclideanDistance(p3) << ", ";
+			cout << p1.getEuclideanDistance(p4);
+			cout << "]\n";
+		}
 
 		if(!p1.isValid() && !p2.isValid() && !p3.isValid() && !p4.isValid()) {
 			return false;
 		}
 
 		return this->l1.isPerpendicular(p.l1)
-			&& (abs(p1.getEuclideanDistance(p2) - p1.getEuclideanDistance(p3)) < numeric_limits<double>::epsilon()
-			|| abs(p1.getEuclideanDistance(p2) - p1.getEuclideanDistance(p4)) < numeric_limits<double>::epsilon());
+			&& (abs(p1.getEuclideanDistance(p2) - p1.getEuclideanDistance(p3)) < EPSILON
+			|| abs(p1.getEuclideanDistance(p2) - p1.getEuclideanDistance(p4)) < EPSILON);
 	}
 
 	toString() {
-		string isParallel = 
-			(abs(l1.getGradient() - l2.getGradient()) < numeric_limits<double>::epsilon()) ? "true" : "false";
 		cout << "Lines: [(";
 		cout << l1.p1.x << ", " << l1.p1.y << "), (" << l1.p2.x << ", " << l1.p2.y;
 		cout << ")], [(";
 		cout << l2.p1.x << ", " << l2.p1.y << "), (" << l2.p2.x << ", " << l2.p2.y;
 		cout << ")]";
 		cout << ", gradients = (" << l1.getGradient() 
-			<< ", " << l2.getGradient() << "), parallel = " << isParallel << "\n";
+			<< ", " << l2.getGradient() << ")" << "\n";
 	}
 };
 
@@ -191,15 +189,29 @@ vector<ParallelLines> findParallelLines(vector<Line> lines) {
 	return p;
 }
 
-// Find all squares given a vector of parallel lines
-int findSquares(vector<ParallelLines> parallelLines) {
-	int count = 0;
+int countSquares(vector<Line> lines) {
+	// Find all pairs of parallel lines given vector of lines
+	vector<ParallelLines> parallelLines = findParallelLines(lines);
+
+	// Print parallel lines
+	// cout << "\nParallel lines: \n";
+	// for(auto &i : parallelLines) {
+	// 	i.toString();
+	// }
+	// cout << "\n";
 
 	// Time complexity: O(n^2)
+	int count = 0;
 	for(int i = 0; i < parallelLines.size(); ++i) {
 		for (int j = i+1; j < parallelLines.size(); ++j) {
 			if(parallelLines[i].isSquare(parallelLines[j])) {
 				count++;
+				if(IS_PRINT_LOGS) {
+					cout << "is a square!\n";
+				}
+			}
+			if(IS_PRINT_LOGS) {
+				cout << "\n";
 			}
 		}
 	}
@@ -211,23 +223,33 @@ int main(int argc, char** argv) {
 	// Initialize points
 	Point p1 { 0.0, 0.0 };
 	Point p2 { 0.0, 1.0 };
+
 	Point p3 { 1.0, 0.0 };
 	Point p4 { 1.0, 1.0 };
 
 	Point p5 { 0.0, 0.0 };
 	Point p6 { 1.0, 0.0 };
+
 	Point p7 { 0.0, 1.0 };
 	Point p8 { 1.0, 1.0 };
 
 	Point p9  { 0.0, 1.0 };
 	Point p10 { 1.0, 0.0 };
+
 	Point p11 { -1.0, 0.0 };
 	Point p12 { 0.0, -1.0 };
 
 	Point p13 { -1.0, 0.0 };
 	Point p14 { 0.0, 1.0 };
+
 	Point p15 { 0.0, -1.0 };
 	Point p16 { 1.0, 0.0 };
+
+	Point p17 { 0.5, -2.0 };
+	Point p18 { 0.5, 2.0 };
+
+	Point p19 { -2.0, 0.5 };
+	Point p20 { 2.0, 0.5 };
 
 	// Lines l1 and l2 are parallel lines p1
 	Line l1 { p1, p2 };
@@ -245,26 +267,16 @@ int main(int argc, char** argv) {
 	Line l7 { p13, p14 };
 	Line l8 { p15, p16 };
 
-	// Initialize lines
+	Line l9 { p17, p18 };
+	Line l10 { p19, p20 };
+
 	// Parallel lines p1 and p2 form a square
 	// Parallel lines p3 and p4 form a square
-	vector<Line> lines { l1, l2, l3, l4, l5, l6, l7, l8 };
+	vector<Line> lines1 { l1, l2, l3, l4, l5, l6, l7, l8 };
+	vector<Line> lines2 { l1, l2, l3, l4, l9, l10 };
 
-	// Find all pairs of parallel lines
-	cout << "Find parallel lines: \n";
-	vector<ParallelLines> parallelLines = findParallelLines(lines);
-	cout << "\n";
-
-	// Print details of parallel lines
-	cout << "Parallel lines: \n";
-	for(auto &i : parallelLines) {
-		i.toString();
-	}
-	cout << "\n";
-
-	// Find all possible squares given a set of parallel lines
-	int numOfSquares = findSquares(parallelLines);
-	cout << "Number of squares: " << numOfSquares;
+	cout << "Total number of squares for lines1: " << countSquares(lines1) << "\n";
+	cout << "Total number of squares for lines2: " << countSquares(lines2) << "\n";
 
 	return 0;
 }
